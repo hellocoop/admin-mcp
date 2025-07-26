16b4b298b7359422b65c70d2793116593bd2b04e
rohanharikr <rohanharikumar80@gmail.com>
2025-07-26 13:47:53 +0100

    comment out debug info

--- src/analytics.js
+++ src/analytics.js
@@ -64,10 +64,10 @@ async function sendPlausibleEvent(url) {
             method: 'POST',
             headers: {
                 // 'User-Agent': `HelloAdminMCP/${admin_mcp_version}`,
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                // 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Content-Type': 'application/json',
-                'X-Forwarded-For': '90.209.47.201', // debug to test a valid ip not localhost
-                'X-Debug-Request': 'true'
+                // 'X-Forwarded-For': '90.209.47.201', // debug to test a valid ip not localhost
+                // 'X-Debug-Request': 'true'
             },
             body: JSON.stringify(eventData)
         });
