4e2465983e6606612f4eaa58cf030ae4023ca331
rohanharikr <rohanharikumar80@gmail.com>
2025-07-26 13:36:26 +0100

    debug ip

--- src/analytics.js
+++ src/analytics.js
@@ -66,7 +66,7 @@ async function sendPlausibleEvent(url) {
                 // 'User-Agent': `HelloAdminMCP/${admin_mcp_version}`,
                 'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
                 'Content-Type': 'application/json',
-                'X-Forwarded-For': '127.0.0.1',
+                'X-Forwarded-For': '90.209.47.201', // debug to test a valid ip not localhost
                 'X-Debug-Request': 'true'
             },
             body: JSON.stringify(eventData)
